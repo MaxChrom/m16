b1a1bf1855265cd938e4b5d25bd57f7df4716cfe
Maxim Khromov <max.khromov.ig@gmail.com>
2022-11-07 14:00:59 +0100

    react installed

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+   
+   
+   @extends('layout.main')
+
+
+   @section('content')
+    <div id="future-react-app">
+        <div id="returned-app-component">
+            <h1>Welcome to MI6</h1>
+        </div>
+    </div>
+   @endsection
+
+   {{-- @section('head')
+       <p>Head</p>
+   @endsection
+
+    @section('body')
+       <p>Head</p>
+   @endsection
+
+    @section('left-arm')
+       <p>Head</p>
+   @endsection
+
+    @section('right-arm')
+       <p>Head</p> --}}
+   {{-- @endsection --}}
