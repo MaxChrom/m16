b1a1dcb370efd1cc1a9bd6b6c9100c2a28690dfa
dnitra <109307365+dnitra@users.noreply.github.com>
2022-11-07 14:01:05 +0100

    section works

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,32 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>MI6</title>
+@extends('layouts.main')
+@section('content')
 
-
-
-    @vite("resources/css/app.scss")
-  </head>
-  <body>
-    <div class="sidebar">
-      <div class="closing-button">
-        <span>◄</span>
-        
-    </div>
-      <img src="./img/mi6-seal.png" alt="" />
-      <nav>
-        <a href=""> Home </a>
-        <a href=""> Poeple of interest </a>
-      </nav>
-    </div>
-
-    <div class="content">
-      <h1>Welcome to MI6</h1>
-    </div>
-
-     @vite('resources/js/app.js')
-  </body>
-</html>
+<h1>Welcom to M16</h1>
+@endsection
