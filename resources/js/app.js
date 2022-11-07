f7e5a755a92bbb1b86c4ea10b874b22abd23ac1f
dnitra <109307365+dnitra@users.noreply.github.com>
2022-11-07 13:41:29 +0100

    created homepage

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+
+const clossingButton = document.querySelector(".closing-button");
+
+const sideBar = document.querySelector(".sidebar");
+
+clossingButton.addEventListener("click", (e) => {
+    e.target.classList.toggle("hidden");
+    sideBar.classList.toggle("hiddenBar");
+});
