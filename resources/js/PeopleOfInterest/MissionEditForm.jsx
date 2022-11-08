e06351ad7ed327730f5bcbba8334820ac441a505
Maxim Khromov <max.khromov.ig@gmail.com>
2022-11-08 15:40:41 +0100

    errors

--- resources/js/PeopleOfInterest/MissionEditForm.jsx
+++ resources/js/PeopleOfInterest/MissionEditForm.jsx
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { useParams } from 'react-router-dom';
-
-function MissionEditForm() {
-    const { missionId } = useParams();
-    const [data, setData] = useState({
-        "name": null,
-        "year": null,
-        "outcome": null
-    });
-
-    const handleChange = (event) => {
-        setValues(previous_values => {
-            return {
-                ...previous_values,
-                [event.target.name]: event.target.value
-            }
-        })
-    }
-
-    return (
-        <form action="" method="post">
-
-            <input type="text" name="name" value={data.name} onChange={handleNameChange} />
-            <input type="number" name="year" value={data.year} onChange={handleNameChange} />
-            <select name="outcome">
-                <option value={true}>Success</option>
-                <option value={false}>Failure</option>
-            </select>
-
-        </form>
-    )
-}
-
-export default MissionEditForm
-=======
 import React from "react";
 import { useParams } from "react-router-dom";
 
@@ -78,4 +41,3 @@ export default function MissionEditForm() {
         </form>
     );
 }
->>>>>>> 704e15bc9c7c88031ec8416fcedc0465f89d4412
