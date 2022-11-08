26aac49e46e1c279e511c53c4df5c074f60e23bc
Maxim Khromov <max.khromov.ig@gmail.com>
2022-11-08 15:50:55 +0100

    pulled

--- resources/js/PeopleOfInterest/MissionEditForm.jsx
+++ resources/js/PeopleOfInterest/MissionEditForm.jsx
@@ -1,48 +1,9 @@
-<<<<<<< HEAD
-import React from 'react'
-import { useParams } from 'react-router-dom';
-
-function MissionEditForm() {
-    const { missionId } = useParams();
-    const [data, setData] = useState({
-        "name": null,
-        "year": null,
-        "outcome": null
-    });
-
-    const handleChange = (event) => {
-        setValues(previous_values => {
-            return {
-                ...previous_values,
-                [event.target.name]: event.target.value
-            }
-        })
-    }
-
-    return (
-        <form action="" method="post">
-
-            <input type="text" name="name" value={data.name} onChange={handleNameChange} />
-            <input type="number" name="year" value={data.year} onChange={handleNameChange} />
-            <select name="outcome">
-                <option value={true}>Success</option>
-                <option value={false}>Failure</option>
-            </select>
-
-        </form>
-    )
-}
-
-export default MissionEditForm
-=======
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 
 export default function MissionEditForm() {
     const { id } = useParams();
-
-    // console.log(missionId);
 
     const [data, setData] = useState({
         name: "",
@@ -60,7 +21,6 @@ export default function MissionEditForm() {
     };
     return (
         <form action="" method="post">
-            {console.log(id)}
             <input
                 type="text"
                 name="name"
@@ -80,4 +40,3 @@ export default function MissionEditForm() {
         </form>
     );
 }
->>>>>>> 704e15bc9c7c88031ec8416fcedc0465f89d4412
